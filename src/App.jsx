a0c8ddab9b8935fa26afeae4b936f4ef5083a359
Adrian Rusan <rusan.adrian.ionut@gmail.com>
2023-10-18 15:10:28 +0300

    small performance fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
-import { CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from './sections'
-
+import { Hero } from './sections'
 import Nav from './components/Nav'
+import React, { Suspense } from 'react'
+
+const PopularProducts = React.lazy(() => import('./sections/PopularProducts'))
+const SuperQuality = React.lazy(() => import('./sections/SuperQuality'))
+const Services = React.lazy(() => import('./sections/Services'))
+const SpecialOffer = React.lazy(() => import('./sections/SpecialOffer'))
+const CustomerReviews = React.lazy(() => import('./sections/CustomerReviews'))
+const Subscribe = React.lazy(() => import('./sections/Subscribe'))
+const Footer = React.lazy(() => import('./sections/Footer'))
 
 const App = () => (
   <main className="relative">
@@ -9,25 +17,39 @@ const App = () => (
       <Hero/>
     </section>
     <section className="padding">
-      <PopularProducts/>
+			<Suspense fallback={<div>Loading</div>}>
+				<PopularProducts />
+			</Suspense>    
     </section>
     <section className="padding">
-      <SuperQuality/>
+			<Suspense fallback={<div>Loading</div>}>
+				<SuperQuality />
+			</Suspense> 
     </section>
     <section className="padding-x py-10">
-      <Services/>
+			<Suspense fallback={<div>Loading</div>}>
+				<Services />
+			</Suspense> 
     </section>
     <section className="padding">
-      <SpecialOffer/>
+			<Suspense fallback={<div>Loading</div>}>
+				<SpecialOffer />
+			</Suspense> 
     </section>
     <section className="bg-pale-blue padding">
-      <CustomerReviews/>
+			<Suspense fallback={<div>Loading</div>}>
+				<CustomerReviews />
+			</Suspense> 
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      <Subscribe/>
+			<Suspense fallback={<div>Loading</div>}>
+				<Subscribe />
+			</Suspense>
     </section>
     <section className="bg-black padding-x padding-t pb-8">
-      <Footer/>
+			<Suspense fallback={<div>Loading</div>}>
+				<Footer />
+			</Suspense> 
     </section>
   </main>
 )
